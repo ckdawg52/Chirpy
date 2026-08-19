fc8ff9f96f7d7904e9aba4314f449c20f970b567
ckdawg52 <ckdawg5250@gmail.com>
2026-08-19 19:47:18 +0000

    Did some stuff

--- main.go
+++ main.go
@@ -4,16 +4,58 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+	
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8", )
+	w.WriteHeader(http.StatusOK)
+	output := fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileserverHits.Load())
+	w.Write([]byte(output))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/htmlc; charset=utf-8", )
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("RESET"))
+}
+
 func main() {
 	fmt.Println("Starting Server . . .")
 
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-
+	fileServer := http.FileServer(http.Dir("."))
+	strippedFileServer := http.StripPrefix("/app", fileServer)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(strippedFileServer))
+	mux.Handle("GET /admin/metrics", http.HandlerFunc(apiCfg.handlerMetrics))
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8", )
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	s := &http.Server{
-		Addr: ":8080",
-		Handler: mux, 
+		Addr:    ":8080",
+		Handler: mux,
 	}
 	log.Fatal(s.ListenAndServe())
 }
