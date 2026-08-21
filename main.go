7bf50ad9180388c30b2bbf44d4e4ecc1ceb3804e
ckdawg52 <ckdawg5250@gmail.com>
2026-08-21 19:17:30 +0000

    Reorganized the code

--- main.go
+++ main.go
@@ -11,51 +11,25 @@ type apiConfig struct {
 fileserverHits atomic.Int32
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-	
-}
-
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8", )
-	w.WriteHeader(http.StatusOK)
-	output := fmt.Sprintf(`<html>
-  <body>
-    <h1>Welcome, Chirpy Admin</h1>
-    <p>Chirpy has been visited %d times!</p>
-  </body>
-</html>`, cfg.fileserverHits.Load())
-	w.Write([]byte(output))
-}
-
-func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/htmlc; charset=utf-8", )
-	w.WriteHeader(http.StatusOK)
-	cfg.fileserverHits.Store(0)
-	w.Write([]byte("RESET"))
-}
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+	apiCfg := apiConfig{}
+
 	fmt.Println("Starting Server . . .")
 
-	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("."))
-	strippedFileServer := http.StripPrefix("/app", fileServer)
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(strippedFileServer))
-	mux.Handle("GET /admin/metrics", http.HandlerFunc(apiCfg.handlerMetrics))
-	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "text/plain; charset=utf-8", )
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", fsHandler)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	s := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(s.ListenAndServe())
 }
